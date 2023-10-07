f8ef3ad331002a84fb5066294d2803518b87d284
HasanMufdy <96997047+HasanMufdy@users.noreply.github.com>
2023-10-07 21:36:13 +0300

    version 1, last added cookie stand will be printed

---
+++ components/Main.tsx
@@ -0,0 +1,10 @@
+const Footer: React.FC = () => {
+    return (
+      <footer className="bg-gray-200 text-center p-4">
+        &copy; {new Date().getFullYear()} Cookie Stand Admin
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
